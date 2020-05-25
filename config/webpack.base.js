327e860896962486e6f437c202a5378bde0ba761
梁振权 <liangzhenquan@liangzhenquandeMacBook-Pro.local>
2020-05-25 19:00:22 +0800

    加入antd主题定制

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -3,12 +3,28 @@
  * @Autor: liang
  * @Date: 2020-05-21 16:55:23
  * @LastEditors: liang
- * @LastEditTime: 2020-05-25 09:20:31
+ * @LastEditTime: 2020-05-25 14:30:13
  */
 const paths = require('./paths');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const cssRegex = /\.css$/;
 const cssModuleRegex = /\.module\.css$/;
+const lessRegex = /\.less$/;
+const modifyVars = {
+  'primary-color': '#7546c9',
+  'link-color': '#1890ff', // 链接色
+  'success-color': '#52c41a', // 成功色
+  'warning-color': '#faad14', // 警告色
+  'error-color': '#f5222d', // 错误色
+  'font-size-base': '14px', // 主字号
+  'heading-color': 'rgba(0, 0, 0, 0.85)', // 标题色
+  'text-color': 'rgba(0, 0, 0, 0.65)', // 主文本色
+  'text-color-secondary': 'rgba(0, 0, 0, 0.45)', // 次文本色
+  'disabled-color': 'rgba(0, 0, 0, 0.25)', // 失效色
+  'border-radius-base': '4px', // 组件/浮层圆角
+  'border-color-base': '#d9d9d9', // 边框色
+  'box-shadow-base': '0 2px 8px rgba(0, 0, 0, 0.15)' // 浮层阴影
+}
 module.exports = function (webpackEnv = "development") {
   const isEnvDevelopment = webpackEnv === "development";
   const isEnvProduction = webpackEnv === "production";
@@ -43,6 +59,23 @@ module.exports = function (webpackEnv = "development") {
         }
       }
     ]
+    if (preProcessor) {
+      loaders.push(
+        {
+          loader: require.resolve("resolve-url-loader")
+        },
+        {
+          loader: require.resolve(preProcessor),
+          options: {
+            sourceMap: true,
+            lessOptions: {
+              modifyVars,
+              javascriptEnabled: true,
+            },
+          },
+        }
+      );
+    }
     return loaders.filter(Boolean);
   }
   return {
@@ -89,7 +122,7 @@ module.exports = function (webpackEnv = "development") {
                   plugins: ['ramda', ['import', {
                     libraryName: "antd",
                     libraryDirectory: "es",
-                    style: 'css', //不用less，则是css，用less，此处为true
+                    style: true, //不用less，则是css，用less，此处为true
                   }]]
                 }
               }
@@ -102,6 +135,13 @@ module.exports = function (webpackEnv = "development") {
                 sourceMap: false
               }),
               sideEffects: true,
+            },
+            {
+              test: lessRegex,
+              use: getStyleLoaders({
+                importLoaders: 3,
+                sourceMap: false
+              }, 'less-loader'),
             },
             {
               loader: require.resolve("file-loader"),
--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@
  * @Autor: liang
  * @Date: 2020-05-21 15:16:56
  * @LastEditors: liang
- * @LastEditTime: 2020-05-25 09:21:15
+ * @LastEditTime: 2020-05-25 15:06:14
  */
 import React, { useState } from "react";
-import { Button } from 'antd';
+import { Button, Slider } from 'antd';
 import 'normalize.css';
+import './index.less';
 function App() {
   const [count, setCount] = useState(0)
   const handleClick = () => {
@@ -16,6 +17,7 @@ function App() {
   return (
     <div className="app">
       <p>点击数: {count}</p>
+      <Slider min={0} max={100} value={count} />
       <Button onClick={handleClick}>点击我</Button>
     </div >
   )
